b1d827f556b6f16a5d36b5beef2101da27aabe5e
邴新科 <kylebing@163.com>
2021-07-24 18:26:02 +0800

    展示分组信息

--- js/Table.js
+++ js/Table.js
@@ -1,16 +1,51 @@
+
 class Table{
-    constructor(dict, tbody) {
-        this.tbody = tbody
-        this.dict = dict
+    /**
+     *
+     * @param yaml yaml 字典表
+     * @param display div.display
+     */
+    constructor(yaml, display) {
+        this.display = display
+        this.yaml = yaml
     }
     showDict(){
-        this.dict.forEach((item, index) => {
-            let tr = this.tbody.querySelector('tr.prototype').cloneNode(true)
-            tr.querySelector('td.id').innerText = index
+        let tbody = this.display.querySelector('tbody')
+        let trOrigin = tbody.querySelector('tr.prototype')
+        tbody.innerHTML = ''
+        this.yaml.dict.forEach((item, index) => {
+            let tr = trOrigin.cloneNode(true)
+            tr.querySelector('td.id').innerText = index + 1
             tr.querySelector('td.code').innerText = item.code || ''
             tr.querySelector('td.word').innerText = item.word || ''
-            this.tbody.appendChild(tr)
+            tbody.appendChild(tr)
         })
+        tbody.removeChild(trOrigin) // 去除 tr.prototype
+    }
+
+    showDictGroup(){
+        let tableOrigin = this.display.querySelector('table.prototype')
+        this.display.innerHTML = ''
+        this.yaml.dictWithGroup.forEach((groupItem, indexGroup) => {
+            let table = tableOrigin.cloneNode(true)
+            let tbody = table.querySelector('tbody')
+            table.querySelector('thead td').innerText = groupItem.groupName // set group name
+
+            // 内层循环
+            let trOrigin = tbody.querySelector('tr.prototype')
+            groupItem.dict.forEach((item, index) => {
+                let tr = trOrigin.cloneNode(true)
+                tr.querySelector('td.id').innerText = index + 1
+                tr.querySelector('td.code').innerText = item.code || ''
+                tr.querySelector('td.word').innerText = item.word || ''
+                tbody.appendChild(tr)
+            })
+            tbody.removeChild(trOrigin) // 去除 tr.prototype
+            // 内层循环 - END
+
+            this.display.appendChild(table)
+        })
+        this.display.removeChild(tableOrigin) // 去除 table.prototype
     }
 }
 export default Table
