2a22d90a00d329a6b993d335a6e654a079605e43
KyleBing <kylebing@163.com>
2021-07-24 22:39:53 +0800

    添加 browser Vue3 支持

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import Dict from "./Dict.js"
 import Table from "./Table.js"
-import { $ } from "./Utility.js"
+import {$} from "./Utility.js"
+
+const Vue = require('vue')
 
 const {ipcRenderer} = require('electron')
 
@@ -9,3 +11,15 @@ ipcRenderer.on('fileHasRead', (event, res) => {
     let table = new Table(dictUser, $('.display'))
     table.showDictGroup()
 })
+
+
+// Vue 3
+const app = {
+    data() {
+        return {
+            name: '名字嘛'
+        }
+    }
+}
+
+Vue.createApp(app).mount('#app')
