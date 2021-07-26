de8b05c425d0ce07a077147dbcbf0650099a5ed4
邴新科 <kylebing@163.com>
2021-07-26 12:27:11 +0800

    词条>1000:手动搜索：自动搜索

--- js/App.js
+++ js/App.js
@@ -14,12 +14,22 @@ const app = {
         return {
             name: '名字嘛',
             dict: null,
-            keyword: ''
+            keyword: '',
+            keywordUnwatch: null // keyword watch 方法的撤消方法
         }
     },
     mounted() {
         ipcRenderer.on('showFileContent', (event, res) => {
             this.dict = new Dict(res)
+            if (this.dict.dict.length > 1000){ // 如果词条数量大于 1000 条，不进行实时筛选
+                if (this.keywordUnwatch){
+                    this.keywordUnwatch()
+                }
+            } else {
+                this.keywordUnwatch = this.$watch('keyword', newValue => {
+                    this.dict.setKeyword(newValue)
+                })
+            }
         })
     },
     methods: {
@@ -27,11 +37,6 @@ const app = {
             this.dict.setKeyword(this.keyword)
         }
     },
-    watch: {
-        keyword(newValue, oldValue){
-            this.dict.setKeyword(newValue)
-        }
-    }
 }
 
 Vue.createApp(app).mount('#app')
