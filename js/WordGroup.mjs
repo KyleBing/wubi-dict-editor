e84e71b1e7bfd62c32b9dd329d3a34c353715a5f
邴新科 <kylebing@163.com>
2021-08-04 14:53:33 +0800

    编辑分组名

--- js/WordGroup.mjs
+++ js/WordGroup.mjs
@@ -3,6 +3,7 @@ class WordGroup{
     constructor(groupName, dict) {
         this.groupName = groupName || ''
         this.dict = dict || []
+        this.isEditingTitle = false // 标题是否在编辑
     }
     // 复制一个对象
     clone(){
